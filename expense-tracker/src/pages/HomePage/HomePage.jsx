e1de7b0559f9e16c866c67d39bb7e720c97984b0
Onkar <ophulwalkar77@gmail.com>
2025-06-06 18:11:45 +0530

    Add: WalletBalance UI, Expense UI

--- expense-tracker/src/pages/HomePage/HomePage.jsx
+++ expense-tracker/src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,39 @@
 import styles from "./HomePage.module.css";
 import ExpenseTracker from "../../sections/ExpenseTracker/ExpenseTracker";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import MyContext from "../../context";
 
 export default function HomePage() {
-    
-    // walletBalance and expenses state
-    const [walletBalance, setWalletBalance] = useState(0);
-    const [expenses, setExpenses] = useState(0);
+  // walletBalance and expenses state
+  const [walletBalance, setWalletBalance] = useState(0);
+  const [expenses, setExpenses] = useState([]);
+
+  //Setting wallet balance to 5k by default on first visit and saving to localStorage
+  useEffect(() => {
+    const storedBalance = localStorage.getItem("walletBalance");
+    const storedExpenses = localStorage.getItem("expenses");
+    if (storedBalance === null) {
+      localStorage.setItem("walletBalance", 5000);
+      setWalletBalance(5000);
+    } else {
+      setWalletBalance(Number(storedBalance));
+    }
+
+    if (storedExpenses === null) {
+      localStorage.setItem("expenses", JSON.stringify([]));
+      setExpenses([]);
+    } else {
+      setExpenses(JSON.parse(storedExpenses));
+    }
+  }, []);
 
   return (
-    <div className={styles.container}>
-      <ExpenseTracker />
-    </div>
+    <MyContext.Provider
+      value={{ walletBalance, setWalletBalance, expenses, setExpenses }}
+    >
+      <div className={styles.container}>
+        <ExpenseTracker />
+      </div>
+    </MyContext.Provider>
   );
 }
